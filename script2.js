1d4344897c02ad47f79e4fa385a3949dc11b3d66
Meadow Green <meadowg.4543@gmail.com>
2021-05-11 15:26:36 -0700

    basic layout of everything
    no formatting no styling

---
+++ script2.js
@@ -0,0 +1,71 @@
+var img1 = document.getElementById('img1');
+var aboutDiv = document.getElementsByClassName('aboutDiv');
+var about = document.getElementById('about');
+let skill1 = document.getElementById('skill1');
+let skill2 = document.getElementById('skill2');
+let skill3 = document.getElementById('skill3');
+
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    var home = document.getElementById('home');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    //home.insertBefore(document, home.childNodes(css));
+    document.body.appendChild(css);
+};
+
+function skillBar(){
+    alert("ah");
+    skill1.style.width = "1rem";
+}
